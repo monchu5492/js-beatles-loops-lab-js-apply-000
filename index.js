35c22a5590f78e343e8e93a6840d283bad397912
Tanner Patterson <tpatterson5492@gmail.com>
2019-09-16 21:25:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments){
+  var bandMembers = []
+  
+  for(var i=0; i<musicians.length; i++){
+    bandMembers.push(musicians[i] + ' plays ' + instruments[i])
+  }
+   return bandMembers
+}
+
+
+
+function johnLennonFacts(array){
+  var theFacts = []
+  
+  var i = 0
+ 
+  while(i<array.length){
+    theFacts.push(array[i] + '!!!')
+    i++
+    }
+    
+     return theFacts
+}
+  
+  function iLoveTheBeatles(num){
+    var love = []
+    
+    do {
+      love.push( "I love the Beatles!" ) 
+   }
+  
+  while (num < 15)
+  num++
+  
+  
+  return love
+  
+  }
+  
